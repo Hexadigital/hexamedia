b81f6fd92727e5b2efb74549328368cb6cd141bc
Hexadigital <57190472+Hexadigital@users.noreply.github.com>
2022-06-11 16:00:03 -0400

    Update configuration and .gitignore

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,6 @@
 AUTHOR = 'Hexadigital'
 SITENAME = 'HexaMedia'
-SITEURL = ''
+SITEURL = 'https://hexa.media'
 
 PATH = 'content'
 
@@ -8,12 +8,20 @@ TIMEZONE = 'America/Detroit'
 
 DEFAULT_LANG = 'en'
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+CATEGORY_FEED_RSS = 'feeds/{slug}.rss.xml'
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
+RSS_FEED_SUMMARY_ONLY = False
+
+BOOTSTRAP_THEME = 'slate'
+JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
+PLUGIN_PATHS = ['../pelican-plugins']
+PLUGINS = ['i18n_subsites']
+THEME = '../pelican-themes/pelican-bootstrap3/'
 
 # Blogroll
 LINKS = (('Pelican', 'https://getpelican.com/'),
@@ -22,10 +30,20 @@ LINKS = (('Pelican', 'https://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+'''SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)'''
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
+
+ARTICLE_URL = '{category}/{slug}/'
+ARTICLE_SAVE_AS = '{category}/{slug}/index.html'
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
+CATEGORY_URL = '{slug}/'
+CATEGORY_SAVE_AS = '{slug}/index.html'
+TAG_URL = 'tagged/{slug}.html'
+TAG_SAVE_AS = 'tagged/{slug}.html'
+AUTHOR_SAVE_AS = ''
