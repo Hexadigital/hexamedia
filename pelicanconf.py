d9cd65bbc4d052f633138b68d74db413dc583585
Hexadigital <57190472+Hexadigital@users.noreply.github.com>
2022-06-11 16:33:28 -0400

    Add about, RSS, socials, and URL rules

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,13 +1,26 @@
+# Core
 AUTHOR = 'Hexadigital'
+BOOTSTRAP_THEME = 'slate'
+DEFAULT_LANG = 'en'
+JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
+PATH = 'content'
+PLUGIN_PATHS = ['../pelican-plugins']
+PLUGINS = ['i18n_subsites']
 SITENAME = 'HexaMedia'
 SITEURL = 'https://hexa.media'
-
-PATH = 'content'
-
+THEME = '../pelican-themes/pelican-bootstrap3/'
 TIMEZONE = 'America/Detroit'
 
-DEFAULT_LANG = 'en'
+# About Me
+ABOUT_ME = 'Hex is a software developer, game reviewer, gardener, music producer, streamer, and more.'
+AVATAR = 'images/avatar.png'
 
+# Blogroll
+LINKS = (
+    ('Pelican', 'https://getpelican.com/'),
+)
+
+# RSS
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 FEED_ALL_RSS = 'feeds/all.rss.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
@@ -17,27 +30,23 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 RSS_FEED_SUMMARY_ONLY = False
 
-BOOTSTRAP_THEME = 'slate'
-JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
-PLUGIN_PATHS = ['../pelican-plugins']
-PLUGINS = ['i18n_subsites']
-THEME = '../pelican-themes/pelican-bootstrap3/'
-
-# Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+# Shariff
+SHARIFF = True
+SHARIFF_LANG = 'en'
+SHARIFF_SERVICES = "['facebook','twitter']"
 
 # Social widget
-'''SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)'''
+SOCIAL = (
+    ('Discord', 'https://discord.gg/a9khY6U'),
+    ('Steam', 'https://store.steampowered.com/curator/34633900/'),
+    ('Twitch', 'https://www.twitch.tv/hexadigital'),
+    ('Twitter', 'https://twitter.com/HexsGameplay'),
+    ('YouTube', 'https://www.youtube.com/c/HexsGameplay'),
+)
 
 DEFAULT_PAGINATION = 10
 
-# Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = True
-
+# URL Rules
 ARTICLE_URL = '{category}/{slug}/'
 ARTICLE_SAVE_AS = '{category}/{slug}/index.html'
 PAGE_URL = '{slug}/'
@@ -47,3 +56,5 @@ CATEGORY_SAVE_AS = '{slug}/index.html'
 TAG_URL = 'tagged/{slug}.html'
 TAG_SAVE_AS = 'tagged/{slug}.html'
 AUTHOR_SAVE_AS = ''
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
