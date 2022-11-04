830c20ea43bed16dceacb762ec730e360c09cb4b
Hexadigital <57190472+Hexadigital@users.noreply.github.com>
2022-11-04 13:49:01 -0400

    Added gameplay for Nier Gestalt (part 8)

---
+++ content/gameplay/A6QXSndBEvQ.md
@@ -0,0 +1,11 @@
+Title: Nier Gestalt (2010) | Hello Kaine [Part 8]
+Date: 2022-11-04 13:49
+Category: Gameplay
+Tags: nier, nier gestalt
+Slug: nier-2010-p8
+Author: Hexadigital
+Summary: Part 8 in a playthrough of Nier Gestalt.
+
+<center><iframe src="https://www.youtube.com/embed/A6QXSndBEvQ?feature=oembed" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" width="640" height="360" frameborder="0"></iframe>
+
+Check out Nier Gestalt on [Remember.Games](https://remember.games/game/2307/nier/)!</center>
--- regen_leaderboard.py
+++ regen_leaderboard.py
@@ -58,9 +58,7 @@ points = {
 print("Regenerating leaderboard...")
 with open('./content/pages/leaderboard.md', 'w') as out_file:
 	# Add page header
-	page = "Title: Twitch Leaderboard\nSummary: See who has attended the most Twitch streams.\nStatus: Hidden\n\nAre you missing from this list? Purchase the Stream Check-In reward during [my streams](https://twitch.tv/hexadigital) and you'll be added next time this is updated.\n\n---\n\nRewards: (subject to approval)  \n10p: Pick a transparent image and I'll put it on my stream for an entire week!  \n25p: Pick a game for me to play and I'll play it for around 30min during a Sunday stream!  \n50p: I'll do a rough pencil sketch of your choice!  \n100p: Make a custom request!  \nTo redeem a reward, send me a message on Twitch or Discord!  \n---\nCurrent drawing queue:  \n1. Anya (NovaPantsu), test drawing  \n2. Hex (similar to given reference), requested by hell_errol  \n---\nLast Updated: "
-	# Add timestamp
-	page += datetime.datetime.now().strftime('%Y-%m-%d, %H:%M Eastern')
+	page = "Title: Twitch Leaderboard\nSummary: See who has attended the most Twitch streams.\nStatus: Hidden\n\nAre you missing from this list? Purchase the Stream Check-In reward during [my streams](https://twitch.tv/hexadigital) and you'll be added next time this is updated.\n\n---\n\nRewards: (subject to approval)  \n10p: Pick a transparent image and I'll put it on my stream for an entire week!  \n25p: Pick a game for me to play and I'll play it for around 30min during a Sunday stream!  \n50p: I'll do a rough pencil sketch of your choice!  \n100p: Make a custom request!  \nTo redeem a reward, send me a message on Twitch or Discord!  \n---\nCurrent drawing queue:  \n1. Anya (NovaPantsu), test drawing  \n2. Hex (similar to given reference), requested by hell_errol  \n---"
 	# Add table header
 	page += '\n\n<table class="table">\n  <thead>\n    <tr>\n      <th scope="col">Rank</th>\n      <th scope="col">Username</th>\n      <th scope="col">Visits</th>\n      <th scope="col">Points</th>\n    </tr>\n  </thead>\n  <tbody>\n'
 	# Group by visits
