fdce48f4ddbcd353d89ce54c0562cc144ba03fe2
Hexadigital <57190472+Hexadigital@users.noreply.github.com>
2023-05-31 21:07:32 -0400

    Added gameplay for Super Squidlit

---
+++ content/gameplay/PaLeHbqF6v8.md
@@ -0,0 +1,11 @@
+Title: Super Squidlit (2021) | Gameplay
+Date: 2023-05-31 21:07
+Category: Gameplay
+Slug: super-squidlit-2021
+Author: Hexadigital
+og_image: images/steam/1813100.jpg
+Appid: 1813100
+
+<center><iframe src="https://www.youtube.com/embed/PaLeHbqF6v8?feature=oembed" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" width="640" height="360" frameborder="0"></iframe>
+
+Check out Super Squidlit on [Steam](https://store.steampowered.com/app/1813100/?curator_clanid=34633900) or [Remember.Games](https://remember.games/game/5014/super-squidlit/)!</center>
--- regen_leaderboard.py
+++ regen_leaderboard.py
@@ -13,9 +13,11 @@ with open('database.json') as lb_file:
 print("Regenerating leaderboard...")
 with open('./content/pages/leaderboard.md', 'w') as out_file:
 	# Add page header
-	page = "Title: Twitch Leaderboard\nSummary: See who has attended the most Twitch streams.\nStatus: Hidden\n\nAre you missing from this list? Purchase the Stream Check-In reward during [my streams](https://twitch.tv/hexadigital) and you'll be added next time this is updated.\n\n---\n\nRewards: (subject to approval)  \n10p: Pick a transparent image and I'll put it on my stream for an entire week!  \n25p: Pick a game for me to play and I'll play it for around 30min during a Sunday stream!  \n50p: Coming Soon  \n100p: Make a custom request!  \nTo redeem a reward, send me a message on Twitch or Discord!  \n---"
+	#page = "Title: Twitch Leaderboard\nSummary: See who has attended the most Twitch streams.\nStatus: Hidden\n\nAre you missing from this list? Purchase the Stream Check-In reward during [my streams](https://twitch.tv/hexadigital) and you'll be added next time this is updated.\n\n---\n\nRewards: (subject to approval)  \n10p: Pick a transparent image and I'll put it on my stream for an entire week!  \n25p: Pick a game for me to play and I'll play it for around 30min during a Sunday stream!  \n50p: Coming Soon  \n100p: Make a custom request!  \nTo redeem a reward, send me a message on Twitch or Discord!  \n---"
+	page = "Title: Twitch Leaderboard\nSummary: See who has attended the most Twitch streams.\nStatus: Hidden\n\nAre you missing from this list? Purchase the Stream Check-In reward during [my streams](https://twitch.tv/hexadigital) and you'll be added next time this is updated.\n\n---"
 	# Add table header
-	page += '\n\n<table class="table">\n  <thead>\n    <tr>\n      <th scope="col">Rank</th>\n      <th scope="col">Username</th>\n      <th scope="col">Visits</th>\n      <th scope="col">Points</th>\n    </tr>\n  </thead>\n  <tbody>\n'
+	#page += '\n\n<table class="table">\n  <thead>\n    <tr>\n      <th scope="col">Rank</th>\n      <th scope="col">Username</th>\n      <th scope="col">Check-Ins</th>\n      <th scope="col">Points</th>\n    </tr>\n  </thead>\n  <tbody>\n'
+	page += '\n\n<table class="table">\n  <thead>\n    <tr>\n      <th scope="col">Rank</th>\n      <th scope="col">Username</th>\n      <th scope="col">Check-Ins</th>\n    </tr>\n  </thead>\n  <tbody>\n'
 	# Group by visits
 	sorted_leaderboard = {}
 	for user in users:
@@ -29,7 +31,8 @@ with open('./content/pages/leaderboard.md', 'w') as out_file:
 		# Sort alphabetically
 		point_users = sorted(sorted_leaderboard[point], key=str.casefold)
 		for user in point_users:
-			page += '    <tr>\n      <th scope="row">%s</th>\n      <td><a href="https://www.twitch.tv/%s">%s</a></td>\n      <td>%s</td>\n      <td>%s</td>\n    </tr>\n' % (rank, user.lower(), user, point, points[user])
+			#page += '    <tr>\n      <th scope="row">%s</th>\n      <td><a href="https://www.twitch.tv/%s">%s</a></td>\n      <td>%s</td>\n      <td>%s</td>\n    </tr>\n' % (rank, user.lower(), user, point, points[user])
+			page += '    <tr>\n      <th scope="row">%s</th>\n      <td><a href="https://www.twitch.tv/%s">%s</a></td>\n      <td>%s</td>\n      </tr>\n' % (rank, user.lower(), user, point)
 		rank += 1
 	page += '  </tbody>\n</table>'
 	out_file.write(page)
